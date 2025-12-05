63b35495e0409e5ba702cc89e2f49e7c1aef9402
Samuel Berthe <dev@samuel-berthe.fr>
2025-12-05 12:21:45 +0100

    Update docusaurus.config.ts

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'ro',
-  tagline: 'Reactive programming for Go',
+  tagline: 'Streams and reactive programming for Go',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
